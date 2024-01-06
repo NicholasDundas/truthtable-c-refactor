97a3ff2dcb0e41e1805d480cc77ae9a2c8b8d8fa
NicholasDundas <nicholasdundas106@gmail.com>
2024-01-05 19:32:09 -0500

    test

--- build/DartConfiguration.tcl
+++ build/DartConfiguration.tcl
@@ -57,9 +57,9 @@ P4UpdateOptions:
 P4UpdateCustom: 
 
 # Generic update command
-UpdateCommand: 
+UpdateCommand: C:/Program Files/Git/cmd/git.exe
 UpdateOptions: 
-UpdateType: 
+UpdateType: git
 
 # Compiler info
 Compiler: C:/msys64/ucrt64/bin/g++.exe
